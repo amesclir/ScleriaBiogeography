19b5a92d1a2e636ce7f1b526e4d29c2efb6608a5
amesclir <amesclir@gmail.com>
2020-01-24 15:33:46 +0100

    ready for publication?

---
+++ BSM_DEC+J/Script3.R
@@ -0,0 +1,329 @@
+.libPaths("/home/aescudero/R/x86_64-pc-linux-gnu-library/3.4")
+
+library(optimx)
+library(FD)
+library(snow)
+library(parallel)
+library(BioGeoBEARS)
+#library(roxygen2)
+
+tr = read.tree("tree.tree")
+tr
+
+trfn = np("tree.tree")
+moref(trfn)
+
+geogfn = np("Scleria_BGB_occurrence_world.txt")
+
+# Look at the raw geography text file:
+moref(geogfn)
+
+# Look at your geographic range data:
+tipranges = getranges_from_LagrangePHYLIP(lgdata_fn=geogfn)
+tipranges
+
+# Set the maximum number of areas any species may occupy; this cannot be larger 
+# than the number of areas you set up, but it can be smaller.
+max_range_size = 7
+
+#######################################################
+# Run DEC+J
+#######################################################
+BioGeoBEARS_run_object = define_BioGeoBEARS_run()
+BioGeoBEARS_run_object$trfn = trfn
+BioGeoBEARS_run_object$geogfn = geogfn
+BioGeoBEARS_run_object$max_range_size = max_range_size
+BioGeoBEARS_run_object$min_branchlength = 0.000001    # Min to treat tip as a direct ancestor (no speciation event)
+BioGeoBEARS_run_object$include_null_range = TRUE    # set to FALSE for e.g. DEC* model, DEC*+J, etc. (see Massana et al.)
+# also search script on "include_null_range" for other places to change
+
+# Set up a time-stratified analysis:
+BioGeoBEARS_run_object$timesfn = "timeperiods.txt"
+BioGeoBEARS_run_object$dispersal_multipliers_fn = "Scleria_BGB_manual_dispersal_multipliers.txt"
+#BioGeoBEARS_run_object$areas_allowed_fn = "areas_allowed.txt"
+#BioGeoBEARS_run_object$areas_adjacency_fn = "areas_adjacency.txt"
+#BioGeoBEARS_run_object$distsfn = "distances_matrix.txt"
+# See notes on the distances model on PhyloWiki's BioGeoBEARS updates page.
+
+# Speed options and multicore processing if desired
+BioGeoBEARS_run_object$speedup = TRUE          # shorcuts to speed ML search; use FALSE if worried (e.g. >3 params)
+BioGeoBEARS_run_object$use_optimx = TRUE     # if FALSE, use optim() instead of optimx()
+BioGeoBEARS_run_object$num_cores_to_use = 1
+BioGeoBEARS_run_object$force_sparse = FALSE    # force_sparse=TRUE causes pathology & isn't much faster at this scale
+
+# This function loads the dispersal multiplier matrix etc. from the text files into the model object. Required for these to work!
+# (It also runs some checks on these inputs for certain errors.)
+BioGeoBEARS_run_object = readfiles_BioGeoBEARS_run(BioGeoBEARS_run_object)
+
+# Divide the tree up by timeperiods/strata (uncomment this for stratified analysis)
+BioGeoBEARS_run_object = section_the_tree(inputs=BioGeoBEARS_run_object, make_master_table=TRUE, plot_pieces=FALSE)
+# The stratified tree is described in this table:
+BioGeoBEARS_run_object$master_table
+
+# Good default settings to get ancestral states
+BioGeoBEARS_run_object$return_condlikes_table = TRUE
+BioGeoBEARS_run_object$calc_TTL_loglike_from_condlikes_table = TRUE
+BioGeoBEARS_run_object$calc_ancprobs = TRUE    # get ancestral states from optim run
+
+# Set up DEC+J model
+# Get the ML parameter values from the 2-parameter nested model
+# (this will ensure that the 3-parameter model always does at least as good)
+dstart = resDEC$outputs@params_table["d","est"]
+estart = resDEC$outputs@params_table["e","est"]
+jstart = 0.0001
+
+# Input starting values for d, e
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["d","init"] = dstart
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["d","est"] = dstart
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["e","init"] = estart
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["e","est"] = estart
+
+# Add j as a free parameter
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["j","type"] = "free"
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["j","init"] = jstart
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["j","est"] = jstart
+
+# Add w as a free parameter
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["w","type"] = "free"
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["w","init"] = 0
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["w","est"] = 0
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["w","min"] = 0
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["w","max"] = 3
+
+check_BioGeoBEARS_run(BioGeoBEARS_run_object)
+
+# For a slow analysis, run once, then set runslow=FALSE to just 
+# load the saved result.
+resfn = "Scleria_DEC+J_M0_constrained_w.Rdata"
+runslow = TRUE
+if (runslow)
+{
+  #sourceall("/Dropbox/_njm/__packages/BioGeoBEARS_setup/")
+  
+  res = bears_optim_run(BioGeoBEARS_run_object)
+  res    
+  
+  save(res, file=resfn)
+  
+  resDECjc = res
+} else {
+  # Loads to "res"
+  load(resfn)
+  resDECjc = res
+}
+
+
+###########################################
+# Pick your model name:
+###########################################
+model_name = "DEC+J_ManDisMul_changethoughtime_w"
+res = resDECjc
+
+#######################################################
+# Plot ancestral states - DEC+J
+#######################################################
+
+#removed
+
+
+#######################################################
+# Stochastic mapping on DEC M3b stratified with islands coming up
+#######################################################
+clado_events_tables = NULL
+ana_events_tables = NULL
+lnum = 0
+
+#######################################################
+# Get the inputs for Biogeographical Stochastic Mapping
+# Note: this can be slow for large state spaces and trees, since 
+# the independent likelihoods for each branch are being pre-calculated
+# E.g., for 10 areas, this requires calculation of a 1024x1024 matrix
+# for each branch.  On a tree with ~800 tips and thus ~1600 branches, this was about 1.6 gigs
+# for storage of "BSM_inputs_file.Rdata".
+# Update: 2015-09-23 -- now, if you used multicore functionality for the ML analysis,
+# the same settings will be used for get_inputs_for_stochastic_mapping().
+#######################################################
+BSM_inputs_fn = "BSM_inputs_file.Rdata"
+runInputsSlow = TRUE
+if (runInputsSlow)
+{
+  stochastic_mapping_inputs_list = get_inputs_for_stochastic_mapping(res=res)
+  save(stochastic_mapping_inputs_list, file=BSM_inputs_fn)
+} else {
+  # Loads to "stochastic_mapping_inputs_list"
+  load(BSM_inputs_fn)
+} # END if (runInputsSlow)
+
+# Check inputs (doesn't work the same on unconstr)
+names(stochastic_mapping_inputs_list)
+stochastic_mapping_inputs_list$phy2
+stochastic_mapping_inputs_list$COO_weights_columnar
+stochastic_mapping_inputs_list$unconstr
+set.seed(seed=as.numeric(Sys.time()))
+
+runBSMslow = TRUE
+if (runBSMslow == TRUE)
+{
+  # Saves to: RES_clado_events_tables.Rdata
+  # Saves to: RES_ana_events_tables.Rdata
+  BSM_output = runBSM(res, stochastic_mapping_inputs_list=stochastic_mapping_inputs_list, maxnum_maps_to_try=100, nummaps_goal=50, maxtries_per_branch=40000, save_after_every_try=TRUE, savedir=getwd(), seedval=12345, wait_before_save=0.01)
+  
+  RES_clado_events_tables = BSM_output$RES_clado_events_tables
+  RES_ana_events_tables = BSM_output$RES_ana_events_tables
+} else {
+  # Load previously saved...
+  
+  # Loads to: RES_clado_events_tables
+  load(file="RES_clado_events_tables.Rdata")
+  # Loads to: RES_ana_events_tables
+  load(file="RES_ana_events_tables.Rdata")
+  BSM_output = NULL
+  BSM_output$RES_clado_events_tables = RES_clado_events_tables
+  BSM_output$RES_ana_events_tables = RES_ana_events_tables
+} # END if (runBSMslow == TRUE)
+
+# Extract BSM output
+clado_events_tables = BSM_output$RES_clado_events_tables
+ana_events_tables = BSM_output$RES_ana_events_tables
+head(clado_events_tables[[1]])
+head(ana_events_tables[[1]])
+length(clado_events_tables)
+length(ana_events_tables)
+
+#######################################################
+# Plot one stochastic map, manual method
+#######################################################
+# (we have to convert the stochastic maps into event
+#  maps for plotting)
+
+######################
+# Get the color scheme
+######################
+include_null_range = TRUE
+areanames = names(tipranges@df)
+areas = areanames
+max_range_size = 7
+
+# Note: If you did something to change the states_list from the default given the number of areas, you would
+# have to manually make that change here as well! (e.g., areas_allowed matrix, or manual reduction of the states_list)
+states_list_0based = rcpp_areas_list_to_states_list(areas=areas, maxareas=max_range_size, include_null_range=include_null_range)
+
+colors_list_for_states = get_colors_for_states_list_0based(areanames=areanames, states_list_0based=states_list_0based, max_range_size=max_range_size, plot_null_range=TRUE)
+
+############################################
+# Setup for painting a single stochastic map
+############################################
+scriptdir = np(system.file("extdata/a_scripts", package="BioGeoBEARS"))
+stratified=TRUE
+clado_events_table = clado_events_tables[[1]]
+ana_events_table = ana_events_tables[[1]]
+
+# cols_to_get = names(clado_events_table[,-ncol(clado_events_table)])
+# colnums = match(cols_to_get, names(ana_events_table))
+# ana_events_table_cols_to_add = ana_events_table[,colnums]
+# anagenetic_events_txt_below_node = rep("none", nrow(ana_events_table_cols_to_add))
+# ana_events_table_cols_to_add = cbind(ana_events_table_cols_to_add, anagenetic_events_txt_below_node)
+# rows_to_get_TF = ana_events_table_cols_to_add$node <= length(tr$tip.label)
+# master_table_cladogenetic_events = rbind(ana_events_table_cols_to_add[rows_to_get_TF,], clado_events_table)
+
+############################################
+# Open a PDF
+############################################
+
+#removed
+
+
+
+#######################################################
+# Plot all 50 stochastic maps to PDF
+#######################################################
+# Setup
+include_null_range = include_null_range
+areanames = areanames
+areas = areanames
+max_range_size = max_range_size
+states_list_0based = rcpp_areas_list_to_states_list(areas=areas, maxareas=max_range_size, include_null_range=include_null_range)
+colors_list_for_states = get_colors_for_states_list_0based(areanames=areanames, states_list_0based=states_list_0based, max_range_size=max_range_size, plot_null_range=TRUE)
+scriptdir = np(system.file("extdata/a_scripts", package="BioGeoBEARS"))
+stratified = stratified
+
+# Loop through the maps and plot to PDF
+
+# removed
+
+
+
+
+#######################################################
+# Summarize stochastic map tables
+#######################################################
+length(clado_events_tables)
+length(ana_events_tables)
+
+head(clado_events_tables[[1]][,-20])
+tail(clado_events_tables[[1]][,-20])
+
+head(ana_events_tables[[1]])
+tail(ana_events_tables[[1]])
+
+areanames = names(tipranges@df)
+actual_names = areanames
+actual_names
+
+# Get the dmat and times (if any)
+dmat_times = get_dmat_times_from_res(res=res, numstates=NULL)
+dmat_times
+
+# Extract BSM output
+clado_events_tables = BSM_output$RES_clado_events_tables
+ana_events_tables = BSM_output$RES_ana_events_tables
+
+# Simulate the source areas
+BSMs_w_sourceAreas = simulate_source_areas_ana_clado(res, clado_events_tables, ana_events_tables, areanames)
+clado_events_tables = BSMs_w_sourceAreas$clado_events_tables
+ana_events_tables = BSMs_w_sourceAreas$ana_events_tables
+
+# Count all anagenetic and cladogenetic events
+counts_list = count_ana_clado_events(clado_events_tables, ana_events_tables, areanames, actual_names)
+
+summary_counts_BSMs = counts_list$summary_counts_BSMs
+print(conditional_format_table(summary_counts_BSMs))
+
+# Histogram of event counts
+hist_event_counts(counts_list, pdffn=paste0(model_name, "_histograms_of_event_counts.pdf"))
+
+#######################################################
+# Print counts to files
+#######################################################
+tmpnames = names(counts_list)
+cat("\n\nWriting tables* of counts to tab-delimited text files:\n(* = Tables have dimension=2 (rows and columns). Cubes (dimension 3) and lists (dimension 1) will not be printed to text files.) \n\n")
+for (i in 1:length(tmpnames))
+{
+  cmdtxt = paste0("item = counts_list$", tmpnames[i])
+  eval(parse(text=cmdtxt))
+  
+  # Skip cubes
+  if (length(dim(item)) != 2)
+  {
+    next()
+  }
+  
+  outfn = paste0(tmpnames[i], ".txt")
+  if (length(item) == 0)
+  {
+    cat(outfn, " -- NOT written, *NO* events recorded of this type", sep="")
+    cat("\n")
+  } else {
+    cat(outfn)
+    cat("\n")
+    write.table(conditional_format_table(item), file=outfn, quote=FALSE, sep="\t", col.names=TRUE, row.names=TRUE)
+  } # END if (length(item) == 0)
+} # END for (i in 1:length(tmpnames))
+cat("...done.\n")
+
+#######################################################
+# Check that ML ancestral state/range probabilities and
+# the mean of the BSMs approximately line up
+#######################################################
+library(MultinomialCI)    # For 95% CIs on BSM counts
+check_ML_vs_BSM(res, clado_events_tables, model_name, tr=NULL, plot_each_node=FALSE, linreg_plot=TRUE, MultinomialCI=TRUE)
---
+++ BSM_DEC/Script4.R
@@ -0,0 +1,246 @@
+.libPaths("/home/aescudero/R/x86_64-pc-linux-gnu-library/3.4")
+
+library(optimx)
+library(FD)
+library(snow)
+library(parallel)
+library(BioGeoBEARS)
+#library(roxygen2)
+
+tr = read.tree("tree.tree")
+tr
+
+trfn = np("tree.tree")
+moref(trfn)
+
+geogfn = np("Scleria_BGB_occurrence_world.txt")
+
+# Look at the raw geography text file:
+moref(geogfn)
+
+# Look at your geographic range data:
+tipranges = getranges_from_LagrangePHYLIP(lgdata_fn=geogfn)
+tipranges
+
+# Set the maximum number of areas any species may occupy; this cannot be larger 
+# than the number of areas you set up, but it can be smaller.
+max_range_size = 7
+
+#######################################################
+# Run DEC
+#######################################################
+
+# Intitialize a default model (DEC model)
+BioGeoBEARS_run_object = define_BioGeoBEARS_run()
+
+# Give BioGeoBEARS the location of the phylogeny Newick file
+BioGeoBEARS_run_object$trfn = trfn
+
+# Give BioGeoBEARS the location of the geography text file
+BioGeoBEARS_run_object$geogfn = geogfn
+
+# Input the maximum range size
+BioGeoBEARS_run_object$max_range_size = max_range_size
+
+BioGeoBEARS_run_object$min_branchlength = 0.000001    # Min to treat tip as a direct ancestor (no speciation event)
+BioGeoBEARS_run_object$include_null_range = TRUE    # set to FALSE for e.g. DEC* model, DEC*+J, etc. (see Massana et al.)
+# also search script on "include_null_range" for other places to change
+
+# Set up a time-stratified analysis:
+# 1. Here, un-comment ONLY the files you want to use.
+# 2. Also un-comment "BioGeoBEARS_run_object = section_the_tree(...", below.
+# 3. For example files see (a) extdata_dir, 
+#  or (b) http://phylo.wikidot.com/biogeobears#files
+#  and BioGeoBEARS Google Group posts for further hints)
+#
+# Uncomment files you wish to use in time-stratified analyses:
+BioGeoBEARS_run_object$timesfn = "timeperiods.txt"
+BioGeoBEARS_run_object$dispersal_multipliers_fn = "Scleria_BGB_manual_dispersal_multipliers.txt"
+#BioGeoBEARS_run_object$areas_allowed_fn = "areas_allowed.txt"
+#BioGeoBEARS_run_object$areas_adjacency_fn = areasadjacency
+#BioGeoBEARS_run_object$distsfn = "distances_matrix.txt"
+# See notes on the distances model on PhyloWiki's BioGeoBEARS updates page.
+
+# Speed options and multicore processing if desired
+BioGeoBEARS_run_object$speedup = TRUE          # shorcuts to speed ML search; use FALSE if worried (e.g. >3 params)
+BioGeoBEARS_run_object$use_optimx = TRUE     # if FALSE, use optim() instead of optimx()
+BioGeoBEARS_run_object$num_cores_to_use = 1
+# (use more cores to speed it up; this requires
+# library(parallel) and/or library(snow). The package "parallel" 
+# is now default on Macs in R 3.0+, but apparently still 
+# has to be typed on some Windows machines. Note: apparently 
+# parallel works on Mac command-line R, but not R.app.
+# BioGeoBEARS checks for this and resets to 1
+# core with R.app)
+
+# Sparse matrix exponentiation is an option for huge numbers of ranges/states (600+)
+# I have experimented with sparse matrix exponentiation in EXPOKIT/rexpokit,
+# but the results are imprecise and so I haven't explored it further.
+# In a Bayesian analysis, it might work OK, but the ML point estimates are
+# not identical.
+# Also, I have not implemented all functions to work with force_sparse=TRUE.
+# Volunteers are welcome to work on it!!
+BioGeoBEARS_run_object$force_sparse = FALSE    # force_sparse=TRUE causes pathology & isn't much faster at this scale
+
+# This function loads the dispersal multiplier matrix etc. from the text files into the model object. Required for these to work!
+# (It also runs some checks on these inputs for certain errors.)
+BioGeoBEARS_run_object = readfiles_BioGeoBEARS_run(BioGeoBEARS_run_object)
+
+# Divide the tree up by timeperiods/strata (uncomment this for stratified analysis)
+BioGeoBEARS_run_object = section_the_tree(inputs=BioGeoBEARS_run_object, make_master_table=TRUE, plot_pieces=FALSE)
+# The stratified tree is described in this table:
+BioGeoBEARS_run_object$master_table
+
+# Good default settings to get ancestral states
+BioGeoBEARS_run_object$return_condlikes_table = TRUE
+BioGeoBEARS_run_object$calc_TTL_loglike_from_condlikes_table = TRUE
+BioGeoBEARS_run_object$calc_ancprobs = TRUE    # get ancestral states from optim run
+
+# Set up DEC model
+# (nothing to do; defaults)
+
+# Look at the BioGeoBEARS_run_object; it's just a list of settings etc.
+BioGeoBEARS_run_object
+
+# This contains the model object
+BioGeoBEARS_run_object$BioGeoBEARS_model_object
+
+# This table contains the parameters of the model 
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table
+
+# Add w as a free parameter
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["w","type"] = "free"
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["w","init"] = 0
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["w","est"] = 0
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["w","min"] = 0
+BioGeoBEARS_run_object$BioGeoBEARS_model_object@params_table["w","max"] = 3
+
+# Run this to check inputs. Read the error messages if you get them!
+check_BioGeoBEARS_run(BioGeoBEARS_run_object)
+
+# For a slow analysis, run once, then set runslow=FALSE to just 
+# load the saved result.
+runslow = TRUE
+resfn = "Scleria_DEC_M0_constrained_w.Rdata"
+if (runslow)
+    {
+    res = bears_optim_run(BioGeoBEARS_run_object)
+    res    
+
+    save(res, file=resfn)
+    resDECc = res
+    } else {
+    # Loads to "res"
+    load(resfn)
+    resDECc = res
+    }
+
+
+
+#####################
+#######################################################
+# Stochastic mapping on DEC with matrix of distances
+#######################################################
+res = res
+#######################################################
+
+clado_events_tables = NULL
+ana_events_tables = NULL
+lnum = 0
+
+#######################################################
+
+BSM_inputs_fn = "BSM_inputs_file.Rdata"
+runInputsSlow = TRUE
+if (runInputsSlow)
+{
+  stochastic_mapping_inputs_list = get_inputs_for_stochastic_mapping(res=res)
+  save(stochastic_mapping_inputs_list, file=BSM_inputs_fn)
+} else {
+  # Loads to "stochastic_mapping_inputs_list"
+  load(BSM_inputs_fn)
+} # END if (runInputsSlow)
+
+# Check inputs (doesn't work the same on unconstr)
+names(stochastic_mapping_inputs_list)
+stochastic_mapping_inputs_list$phy2
+stochastic_mapping_inputs_list$COO_weights_columnar
+stochastic_mapping_inputs_list$unconstr
+set.seed(seed=as.numeric(Sys.time()))
+
+runBSMslow = TRUE
+if (runBSMslow == TRUE)
+{
+  # Saves to: RES_clado_events_tables.Rdata
+  # Saves to: RES_ana_events_tables.Rdata
+  BSM_output = runBSM(res, stochastic_mapping_inputs_list=stochastic_mapping_inputs_list, maxnum_maps_to_try=100, nummaps_goal=50, maxtries_per_branch=40000, save_after_every_try=TRUE, savedir=getwd(), seedval=12345, wait_before_save=0.01)
+  
+  RES_clado_events_tables = BSM_output$RES_clado_events_tables
+  RES_ana_events_tables = BSM_output$RES_ana_events_tables
+} else {
+  # Load previously saved...
+  
+  # Loads to: RES_clado_events_tables
+  load(file="RES_clado_events_tables.Rdata")
+  # Loads to: RES_ana_events_tables
+  load(file="RES_ana_events_tables.Rdata")
+  BSM_output = NULL
+  BSM_output$RES_clado_events_tables = RES_clado_events_tables
+  BSM_output$RES_ana_events_tables = RES_ana_events_tables
+} # END if (runBSMslow == TRUE)
+
+save(BSM_output, file = "BSM_output.RData")
+
+# Extract BSM output
+clado_events_tables = BSM_output$RES_clado_events_tables
+ana_events_tables = BSM_output$RES_ana_events_tables
+head(clado_events_tables[[1]])
+head(ana_events_tables[[1]])
+length(clado_events_tables)
+length(ana_events_tables)
+
+include_null_range = TRUE
+areanames = names(tipranges@df)
+areas = areanames
+max_range_size = 8
+
+
+
+#######################################################
+# Summarize stochastic map tables
+#######################################################
+length(clado_events_tables)
+length(ana_events_tables)
+
+head(clado_events_tables[[1]][,-20])
+tail(clado_events_tables[[1]][,-20])
+
+head(ana_events_tables[[1]])
+tail(ana_events_tables[[1]])
+
+areanames = names(tipranges@df)
+actual_names = areanames
+actual_names
+
+# Get the dmat and times (if any)
+dmat_times = get_dmat_times_from_res(res=res, numstates=NULL)
+dmat_times
+
+# Extract BSM output
+clado_events_tables = BSM_output$RES_clado_events_tables
+ana_events_tables = BSM_output$RES_ana_events_tables
+
+# Simulate the source areas
+BSMs_w_sourceAreas = simulate_source_areas_ana_clado(res, clado_events_tables, ana_events_tables, areanames)
+clado_events_tables = BSMs_w_sourceAreas$clado_events_tables
+ana_events_tables = BSMs_w_sourceAreas$ana_events_tables
+
+# Count all anagenetic and cladogenetic events
+counts_list = count_ana_clado_events(clado_events_tables, ana_events_tables, areanames, actual_names)
+
+save(counts_list, file = "counts_list.RData")
+
+summary_counts_BSMs = counts_list$summary_counts_BSMs
+print(conditional_format_table(summary_counts_BSMs))
+
+
